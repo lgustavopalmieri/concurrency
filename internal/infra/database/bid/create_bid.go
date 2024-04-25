d7f7e27867afd3ba6e69ce751184e217c83ed667
lgustavopalmieri <lgustavopalmieri@gmail.com>
2024-04-25 06:59:48 -0300

    feat: implements find auctions

---
+++ internal/infra/database/bid/create_bid.go
@@ -0,0 +1,63 @@
+package bid
+
+import (
+	"context"
+	"sync"
+
+	"github.com/lgustavopalmieri/labs-go-expert-auctiont/configuration/logger"
+	"github.com/lgustavopalmieri/labs-go-expert-auctiont/internal/entity/auction_entity"
+	"github.com/lgustavopalmieri/labs-go-expert-auctiont/internal/entity/bid_entity"
+	"github.com/lgustavopalmieri/labs-go-expert-auctiont/internal/infra/database/auction"
+	"github.com/lgustavopalmieri/labs-go-expert-auctiont/internal/internal_error"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BidEntityMongo struct {
+	BidId     string  `bson:"_bid_id"`
+	UserId    string  `bson:"user_id"`
+	AuctionId string  `bson:"auction_id"`
+	Amount    float64 `bson:"amount"`
+	Timestamp int64   `bson:"timestamp"`
+}
+
+type BidRepository struct {
+	Collection        *mongo.Collection
+	AuctionRepository *auction.AuctionRepository
+}
+
+func (bd *BidRepository) CreateBid(ctx context.Context, bidEntities []bid_entity.Bid) *internal_error.InternalError {
+	var wg sync.WaitGroup
+
+	for _, bid := range bidEntities {
+		wg.Add(1)
+		go func(bidValue bid_entity.Bid) {
+			defer wg.Done()
+
+			auctionEntity, err := bd.AuctionRepository.FindAuctionById(ctx, bidValue.AuctionId)
+			if err != nil {
+				logger.Error("Error trying to find auction by id for bid", err)
+				return 
+			}
+
+			if auctionEntity.Status != auction_entity.Active{
+				return
+			}
+
+			bidEntityMongo := &BidEntityMongo{
+				BidId: bidValue.BidId,
+				UserId: bidValue.UserId,
+				AuctionId: bidValue.AuctionId,
+				Amount: bidValue.Amount,
+				Timestamp: bidValue.Timestamp.Unix(),
+			}
+
+			if _, err := bd.Collection.InsertOne(ctx, bidEntityMongo); err != nil {
+				logger.Error("Error trying to insert bid", err)
+				return 
+			}
+		}(bid)
+	}
+
+	wg.Wait()
+	return nil
+}
